6da74b62ba1b2329bc532b4390a179cbf7ee0c00
sandalur <sandalur@asu.edu>
2022-10-27 15:32:37 -0700

    created methods and called them in main

---
+++ src/urinals.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+// author: Srilakshmi Sravani Andaluri
+// sandalur
+public class urinals {
+    static void getString() {
+        System.out.println("First Method not implemented!");
+    }
+    static void openFile() {
+        System.out.println("Second Method not implemented!");
+    }
+    static void countUrinals() {
+        System.out.println("Third Method not implemented!");
+    }
+
+    public static void main(String[] args) {
+        getString();
+        openFile();
+        countUrinals();
+    }
+}
+
