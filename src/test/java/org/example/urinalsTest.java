d07d8f093d456430ead54eb04988875c103a67a0
sandalur <sandalur@asu.edu>
2022-10-28 22:27:39 -0700

    Final

--- src/test/java/org/example/urinalsTest.java
+++ src/test/java/org/example/urinalsTest.java
@@ -1,14 +1,20 @@
 package org.example;
 import org.junit.jupiter.api.Test;
 import java.io.File;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SuppressWarnings("ALL")
 class urinalsTest {
-    urinals test_object=new urinals();
+    urinals test_object = new urinals();
 
     @Test
     void getString() {
+    }
+
+    @Test
+    void main() {
     }
 
     @Test
@@ -17,15 +23,15 @@ class urinalsTest {
         File file = new File("somefile.txt");
         boolean expectedRes = false;
         boolean actualRes = false;
-        if (file.exists()){
+        if (file.exists()) {
             actualRes = true;
         }
         assertEquals(expectedRes, actualRes);
         System.out.println("test for empty file");
-        File file1 = new File("urinals.dat.txt");
+        File file1 = new File("randomfile.txt");
         boolean expected1 = false;
-        boolean actual1= false;
-        if (file1==null){
+        boolean actual1 = false;
+        if (file1 == null) {
             actual1 = true;
         }
         assertEquals(expected1, actual1);
@@ -34,17 +40,17 @@ class urinalsTest {
     @Test
     void countUrinals() {
         System.out.println("====== Srilakshmi Sravani Andaluri == TEST THREE EXECUTED =======");
-        assertEquals( -1,urinals.countUrinals("0110"));
-        assertEquals( 3,urinals.countUrinals("00000"));
-        assertEquals( 1,urinals.countUrinals("01000"));
-        assertEquals( -1,urinals.countUrinals("011"));
-        assertEquals( -1,urinals.countUrinals("01A1"));
-        assertEquals( 0,urinals.countUrinals("1001"));
+        assertEquals(-1, urinals.countUrinals("0110"));
+        assertEquals(3, urinals.countUrinals("00000"));
+        assertEquals(1, urinals.countUrinals("01000"));
+        assertEquals(-1, urinals.countUrinals("011"));
+        assertEquals(-1, urinals.countUrinals("01A1"));
+        assertEquals(0, urinals.countUrinals("1001"));
     }
 
     @Test
     void goodString() {
-        assertEquals( true, urinals.goodString("1010"));
+        assertEquals(true, urinals.goodString("1010"));
         System.out.println("====== Srilakshmi Sravani Andaluri == TEST ONE EXECUTED =======");
         assertEquals(false, urinals.goodString("1100"));
         System.out.println("====== Srilakshmi Sravani Andaluri == TEST TWO EXECUTED =======");
@@ -53,6 +59,15 @@ class urinalsTest {
     }
 
     @Test
-    void main() {
+    void createFiles() throws IOException {
+        System.out.println("test for IO Exception");
+        File file = new File("randomfile.txt");
+        boolean expectedRes = false;
+        boolean actualRes = false;
+        if (file.exists()) {
+            actualRes = true;
+        }
+        assertEquals(expectedRes, actualRes);
+
     }
 }
